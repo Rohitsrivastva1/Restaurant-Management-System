67ee71ce4f0dca06ddae7c51735ca6c64a8c0fd7
rohitsrivastva1 <rohit.managers@gmail.com>
2024-12-12 21:55:56 +0530

    Remove db

--- db.py
+++ db.py
@@ -1,9 +1,9 @@
 # db.py
 
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# import os
 
 # DATABASE_URL = "postgresql://rsadmin:fGA0SHmsnvbTNj4BbAIwZf2HKqUusPpS@dpg-cspn45e8ii6s73bq3jc0-a/qrcodedb_a0yh"
 # engine = create_engine(DATABASE_URL)
